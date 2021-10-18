3127eeec218feba7768a8c8b4fe624ec5f788a32
Loeka Lievens <loekalievens@hotmail.com>
2021-10-18 22:38:15 +0200

    Better conversion between everything

--- src/me/loeka/Main.java
+++ src/me/loeka/Main.java
@@ -3,59 +3,93 @@ package me.loeka;
 import me.loeka.errors.IncorrectInputException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("What conversion do you want to do?");
+    private static ArrayList<String> options;
 
+    public static void main(String[] args) {
         var classes = Conversion.class.getClasses();
 
-        for (int i = 0; i < classes.length; i++) {
-            System.out.printf("[%d]: %s\n", i, conversionToConsoleOption(classes[i].getSimpleName()));
+        options = new ArrayList<>();
+        for (Class<?> c : classes) {
+            var splitted = c
+                    .getSimpleName()
+                    .toLowerCase(Locale.ROOT)
+                    .split("to");
+            if (!options.contains(splitted[0]))
+                options.add(splitted[0]);
+            if (!options.contains(splitted[1]))
+                options.add(splitted[1]);
         }
 
-        var scanner = new Scanner(System.in);
-        var option = scanner.nextInt();
-
-        var selectedConversion = classes[option];
-        if (selectedConversion.getInterfaces()[0].getSimpleName() != IConversion.class.getSimpleName()) {
-            System.out.println("Oops, something went wrong...");
-            System.exit(1);
-        }
-
-        Object instantiatedConversionClass = null;
-        try {
-            instantiatedConversionClass = selectedConversion.getConstructor().newInstance();
-        } catch (Exception e) {
-            endProgramWithErrorMessage();
-        }
+        System.out.println("From what type do you want to convert:");
+        var from = selectOption();
+        System.out.println("To what type do you want to convert:");
+        var to = selectOption();
 
         System.out.println("Enter the value you want to convert:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Object output = null;
+        Object result = null;
         while (true) {
             try {
-                Object converted = getMethodFromConversionClass(selectedConversion, "ParseInputToParameter")
-                        .invoke(instantiatedConversionClass, br.readLine());
-                output = getMethodFromConversionClass(selectedConversion, "Convert")
-                        .invoke(instantiatedConversionClass, converted);
+                var input = br.readLine();
+                if (from.equals(to)) {
+                    result = input;
+                } else if (from.equals("Binary") || to.equals("Binary")) {
+                    result = invokeConversionInClass(from + "To" + to, input);
+                } else {
+                    var binary = invokeConversionInClass(from + "ToBinary", input);
+                    result = invokeConversionInClass("BinaryTo" + to, binary.toString());
+                }
                 break;
             } catch (Exception e) {
-                if (e instanceof IOException || e.getCause() instanceof IncorrectInputException) {
-                    System.out.println("Something went wrong while reading your input, please try again...");
+                if (e.getCause() instanceof IncorrectInputException) {
+                    System.out.println("Incorrect input data, please try again...");
                     continue;
                 }
+
                 endProgramWithErrorMessage();
             }
         }
 
-        System.out.printf("Converted value: %s", output);
+        System.out.printf("The result is: %s", result);
+    }
+
+    private static Object invokeConversionInClass(String className, String input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        var convertClass = getClassFromConversion(className);
+        var instance = convertClass.getConstructor().newInstance();
+        Object converted = getMethodFromConversionClass(convertClass, "ParseInputToParameter")
+                .invoke(instance, input);
+        return getMethodFromConversionClass(convertClass, "Convert")
+                .invoke(instance, converted);
+    }
+
+    private static String selectOption() {
+        for (int i = 0; i < options.stream().count(); i++) {
+            System.out.printf("[%d]: %s\n", i, capitalizeFirstLetter(options.get(i)));
+        }
+
+        var scanner = new Scanner(System.in);
+        var selectedOptions = scanner.nextInt();
+        return capitalizeFirstLetter(options.get(selectedOptions));
+    }
+
+    private static String capitalizeFirstLetter(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
+    private static Class<?> getClassFromConversion(String name) throws ClassNotFoundException {
+        for (Class<?> c : Conversion.class.getClasses())
+            if (c.getSimpleName().equals(name)) return c;
+
+        throw new ClassNotFoundException();
     }
 
     private static Method getMethodFromConversionClass(Class<?> conversion, String methodName) throws NoSuchMethodException {
@@ -68,9 +102,5 @@ public class Main {
     private static void endProgramWithErrorMessage() {
         System.out.println("Oops, something went wrong...");
         System.exit(1);
-    }
-
-    private static String conversionToConsoleOption(String s) {
-        return String.join(" -> ", s.toLowerCase(Locale.ROOT).split("to"));
     }
 }
